5694ba18add08bfe4bcae7b03f499debd78a3ab4
Seungyeon Ji <jiseungyeon0321@gmail.com>
2024-12-03 15:22:25 -0800

    feat: completed week3 project

--- week3/starting-project/App.js
+++ week3/starting-project/App.js
@@ -4,6 +4,7 @@ import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealOverviewScreen from "./screens/MealOverviewScreen";
+import MealScreen from "./screens/MealScreen";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -13,9 +14,20 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "white" },
+          headerTintColor: "black",
+          contentStyle: { backgroundColor: "gray" },
+        }}
+      >
         <Stack.Screen name="Categories" component={CategoriesScreen} />
-        <Stack.Screen name="MealOverview" component={MealOverviewScreen} />
+        <Stack.Screen
+          name="MealOverview"
+          component={MealOverviewScreen}
+          options={{ title: "Meal Overview" }}
+        />
+        <Stack.Screen name="MealScreen" component={MealScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- week3/starting-project/screens/MealOverviewScreen.js
+++ week3/starting-project/screens/MealOverviewScreen.js
@@ -1,17 +1,48 @@
 import { FlatList, Text } from "react-native";
 import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
+import { useLayoutEffect } from "react";
+import { CATEGORIES } from "../data/dummy-data";
 
-function MealOverviewScreen({ route }) {
+function MealOverviewScreen({ route, navigation }) {
   const { categoryId } = route.params;
-  console.log(categoryId);
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(categoryId) >= 0;
+  });
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: CATEGORIES.find((category) => category.id === categoryId).title,
+    });
+  }, [categoryId, navigation]);
 
   function renderMealItem(itemData) {
-    return <Text>{itemData.item.title}</Text>;
+    const item = itemData.item;
+
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      complexity: item.complexity,
+      affordability: item.affordability,
+    };
+
+    return (
+      <MealItem
+        {...mealItemProps}
+        onPress={() =>
+          navigation.navigate("MealScreen", {
+            mealId: item.id,
+          })
+        }
+      />
+    );
   }
 
   return (
     <FlatList
-      data={MEALS}
+      data={displayedMeals}
       keyExtractor={(itemData) => itemData.id}
       renderItem={renderMealItem}
       numColumns={2}
